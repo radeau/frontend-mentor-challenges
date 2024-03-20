93269a1ed85152f5ad5c3e7360f7967b0cd0ed37
radeau <radeau@adso-rad>
2024-03-20 13:56:17 +0400

    Update font family to Figtree

--- blog-preview-card-main/app/layout.tsx
+++ blog-preview-card-main/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Figtree } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Figtree({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Blog Preview Card",
