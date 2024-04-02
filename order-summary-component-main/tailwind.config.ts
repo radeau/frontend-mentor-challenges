2f055a52763782ddf9058b50c6de7f23e035c8aa
radeau <radeau@adso-rad>
2024-04-02 12:02:12 +0400

    Build: customized tailwind config update styling install next-unused packaged

--- order-summary-component-main/tailwind.config.ts
+++ order-summary-component-main/tailwind.config.ts
@@ -1,4 +1,6 @@
+import { url } from "inspector";
 import type { Config } from "tailwindcss";
+// import imagesss from "./images/pattern-background-desktop.svg"
 
 const config: Config = {
   content: [
@@ -10,9 +12,19 @@ const config: Config = {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "bg-pattern-desktop": "url('./images/pattern-background-desktop.svg')"
       },
+      colors: {
+        "pale-blue": "hsl(225, 100%, 94%)",
+        "bright-blue": "hsl(245, 75%, 52%)",
+        "very-pale-blue": "hsl(225, 100%, 98%)",
+        "desaturated-blue": "hsl(224, 23%, 55%)",
+        "dark-blue": "hsl(223, 47%, 23%)"
+      },
+      fontFamily: {
+        'red-hat': ['Red Hat Display', 'sans-serif',],
+      }
     },
   },
   plugins: [],
